9eec0c2cafc6cdd2eb621d8a3922865b7f37555c
cleristonnovaes <67427759+Dz7-Telecom@users.noreply.github.com>
2022-05-04 11:32:01 -0300

    Challenge 1 - accomplishment

---
+++ Desafios/Desafio1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste_de_Klingon
+{
+    public class Desafio1
+    {
+        private readonly string _foo = "slfwek";
+        
+        public int GetProposicoes(string texto)
+        {
+            var qtde = 0;
+
+            foreach (var palavra in texto.Split(" "))
+            {
+                var lastWord = palavra[palavra.Length - 1];
+                if(palavra.Length == 3 && !_foo.Contains(lastWord) && !palavra.Contains("d"))
+                {
+                    qtde++;
+                }
+
+            }
+            return qtde;
+        }
+       
+      
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.IO;
 
 namespace Teste_de_Klingon
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Desafio1 desafio1 = new Desafio1();
+            TextReader textoB = new StreamReader("klingon-textoB.txt");
+            var questao1 = desafio1.GetProposicoes(textoB.ReadToEnd()); 
+            Console.WriteLine(questao1);
+
+            Console.ReadKey();
+
         }
     }
 }
