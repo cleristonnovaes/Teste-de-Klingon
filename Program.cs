40a2c6db90cfbf056c956d895b36b9b2070551a6
cleristonnovaes <67427759+Dz7-Telecom@users.noreply.github.com>
2022-05-04 10:02:27 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Teste_de_Klingon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
